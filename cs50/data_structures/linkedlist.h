f5ed53b919abc1449ddd95eef4f9d4c0bd56add1
Alvaro Duran <alvaro.duranb@gmail.com>
2019-09-12 18:33:31 +0200

    memory

---
+++ cs50/data_structures/linkedlist.h
@@ -0,0 +1,7 @@
+//
+typedef struct node
+{
+  int number;
+  struct node *next;
+}
+node;
---
+++ cs50/memory/garbage_value.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+  char a[] = {'a', 'b', 'c'};
+
+  // printf("%c\n", a[0]);
+  printf("%c\n", a[5]);
+}
